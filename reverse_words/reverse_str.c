cacbc6e801fe1b75f23987b2e24f3ab8accbbc80
TheOrangeRider <theorangerider@users.noreply.github.com>
2015-06-15 21:31:56 -0700

    Added reverse_words.c

---
+++ reverse_words/reverse_str.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+// To execute C, please define "int main()"
+//"dog"
+//"god"
+
+void reverse_string(char * str, int len)
+{
+    char temp;
+    int i = 0;
+    for (i = 0; i < len/2; i++)
+    {
+      temp = str[i];
+      str[i] = str[len-1-i];
+      str[len-1-i] = temp;
+    }
+}
+
+int main() {
+  
+    char my_str[1024];
+    strncpy(my_str, "To execute C, please define \"int main()\"", 1024);
+
+    // First reverse entire string
+    reverse_string(my_str, strlen(my_str));
+    // for each word, reverse the word
+    int i = 0;
+    int start_idx = 0;
+
+    while (i < strlen(my_str))
+    {
+        // The next group of letters could be either alpha-characters or
+        // non-alpha characters. Find which one and then reverse that group
+        int is_alpha = isalpha(my_str[i]);
+        start_idx = i;
+        while (is_alpha == isalpha(my_str[i]))
+        {
+            i++;
+        }
+        // Found a group of alpha or non-alpha, reverse it
+        printf("Reversing my_str\n %s\n from start_idx %d for length of %d."
+                "i is %d\n",
+               my_str, start_idx, i-start_idx, i);
+        reverse_string(&my_str[start_idx], i - start_idx);
+        // Move on to the next character
+    }
+    printf("%s\n", my_str);
+    return 0;
+}
+
